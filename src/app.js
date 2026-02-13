b57e49912f854392ef25da6a4d6f584a858037ce
cvm1912 <rajshivam1912@gmail.com>
2026-02-13 09:53:51 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import authRoutes from './routes/authRoutes.js'
 import notificationRoutes from './routes/notificationRoutes.js'
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 
